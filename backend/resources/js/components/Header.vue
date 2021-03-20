61baa4c6d5fce6516e377700cbfa3098db8243ff
kbys1025 <kbys1025@gmail.com>
2021-03-20 16:23:39 +0900

    リンク修正

--- backend/resources/js/components/Header.vue
+++ backend/resources/js/components/Header.vue
@@ -1,33 +1,33 @@
 <template>
     <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
         <div class="container">
-            <a class="navbar-brand" href="/">Laravel-Vue-Blog</a>
+            <router-link to="/" class="navbar-brand">Laravel-Vue-Blog</router-link>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item">
-                        <a class="nav-link" href="/blog">ブログ一覧</a>
+                        <router-link to="/blog" class="nav-link">ブログ一覧</router-link>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/user">ユーザー一覧</a>
+                        <router-link to="/user" class="nav-link">ユーザー一覧</router-link>
                     </li>
                     <li class="nav-item dropdown" v-if="isLogin">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             {{ username }}
                         </a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <a class="dropdown-item" href="#">マイブログ一覧</a>
+                            <router-link to="#" class="dropdown-item">マイブログ一覧</router-link>
                             <a class="dropdown-item" href="#" @click.prevent="logout">ログアウト</a>
                         </div>
                     </li>
                     <template v-else>
                         <li class="nav-item">
-                            <a class="nav-link" href="/register">ユーザー登録</a>
+                            <router-link to="/register" class="nav-link">ユーザー登録</router-link>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/login">ログイン</a>
+                            <router-link to="/login" class="nav-link">ログイン</router-link>
                         </li>
                     </template>
                 </ul>
